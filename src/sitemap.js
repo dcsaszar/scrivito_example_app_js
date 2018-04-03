d44e99241fdc2d253061fbfbbeb67d0b2490de9c
Alexander Pepper <alexander.pepper@infopark.de>
2018-04-03 10:39:01 +0200

    sitemap.js: Added needed symbol polyfill
    Otherwise prerendering may not work correctly.

--- src/sitemap.js
+++ src/sitemap.js
@@ -1,3 +1,7 @@
+// Needed polyfill to support an iterable ObjSearch in older browsers.
+// See https://www.scrivito.com/objsearch-aaa67c3157464d01
+import 'core-js/modules/es6.symbol';
+
 import * as Scrivito from 'scrivito';
 import './Objs';
 import './config';
