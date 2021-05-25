300fbc1b032b92e6dfecaaa3630397df0f0882f9
David Császár <dcsaszar@users.noreply.github.com>
2021-05-25 20:56:09 +0200

    Autofix example: make `b` and `i` semantic
    * Abusing the validation API.
    * Use at your own risk.

--- src/Widgets/TextWidget/TextWidgetEditingConfig.js
+++ src/Widgets/TextWidget/TextWidgetEditingConfig.js
@@ -23,4 +23,33 @@ Scrivito.provideEditingConfig("TextWidget", {
   initialContent: {
     alignment: "left",
   },
+  validations: [["text", validateAndFixSemanticHtml]],
 });
+
+let validateTagsTimeoutId;
+
+function validateAndFixSemanticHtml(html, { widget }) {
+  if (html !== semanticHtml(html)) {
+    // prevent update flood in case of many changes:
+    clearTimeout(validateTagsTimeoutId);
+
+    // autofix workaround:
+    validateTagsTimeoutId = setTimeout(() => {
+      widget.update({ text: semanticHtml(widget.get("text")) });
+    }, 0);
+
+    // safety net: if autofixing misses an instance, still output a warning:
+    return {
+      severity: "warning",
+      message: "The HTML contains a non-semantic tag.",
+    };
+  }
+}
+
+function semanticHtml(html) {
+  return html
+    .replace("<b>", "<strong>")
+    .replace("</b>", "</strong>")
+    .replace("<i>", "<em>")
+    .replace("</i>", "</em>");
+}
